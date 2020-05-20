91e9e657fd9a7adb03925a8798daf8ca40937155
mxchacha <mxchacha@espe.edu.ec>
2020-05-20 11:14:42 -0500

    Primer Commit

---
+++ src/test/java/com/chacha/ejercicio/PryEjercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chacha.ejercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PryEjercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
